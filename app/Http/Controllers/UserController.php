e79a3290119696aad2b69e3ba0b7713e186bd9ba
Aldryn Gutierrez <aldryn.gutierrez@gmail.com>
2019-10-28 12:30:16 +0800

    Add JWT Authentication + User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use  App\User;
+
+class UserController extends Controller
+{
+     /**
+     * Instantiate a new UserController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Get all User.
+     *
+     * @return Response
+     */
+    public function get()
+    {
+         return response()->json(['users' =>  User::all()], 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('register', 'AuthController@register');
+    $router->post('login', 'AuthController@login');
+
+    $router->get('users', 'UserController@get');
+});
