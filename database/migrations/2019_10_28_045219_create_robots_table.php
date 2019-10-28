5a5a9275c65cf51b81374078dbe0e46f443b616c
Aldryn Gutierrez <aldryn.gutierrez@gmail.com>
2019-10-28 13:05:25 +0800

    Add Robot and UserRobot Model/Migration

---
+++ database/migrations/2019_10_28_045219_create_robots_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRobotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('robots', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('weight', 8, 2);
+            $table->decimal('power', 8, 2);
+            $table->decimal('speed', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('robots');
+    }
+}
